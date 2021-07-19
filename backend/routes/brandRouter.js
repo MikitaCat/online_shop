83918224feedae30169fe6cff16e822e43d1cef1
MikitaCat <nikita.proger21@gmail.com>
2021-07-20 01:04:24 +0300

    typeController was updated

--- backend/routes/brandRouter.js
+++ backend/routes/brandRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express');
-const router = new Router();
 const brandController = require('../controllers/userController');
+const router = new Router();
 
 router.post('/', () => brandController.create);
 router.get('/', () => brandController.getAll);
