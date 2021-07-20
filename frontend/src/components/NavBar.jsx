d59f46c65f367c9b92d543b348d9792bc19d95d8
MikitaCat <nikita.proger21@gmail.com>
2021-07-21 00:24:01 +0300

    Navbar was created with usings react-bootstrap

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { Context } from '..';
+import Navbar from 'react-bootstrap/Navbar';
+import { observer } from 'mobx-react-lite';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import { SHOP_ROUTE } from '../utils/constants';
+
+const NavBar = observer(() => {
+  const { user } = useContext(Context);
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <NavLink to={SHOP_ROUTE} style={{ color: 'white' }}>
+          BuyDevice
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Button variant={'outline-light'}>For Admins</Button>
+            <Button variant={'outline-light'} style={{ marginLeft: '10px' }}>
+              Exit
+            </Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>
+              Authorization
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  );
+});
+
+export default NavBar;
