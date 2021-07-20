67366a0ba6ce5701c09b48053c2770bf5a1d504e
MikitaCat <nikita.proger21@gmail.com>
2021-07-20 16:24:33 +0300

    !!!Authorisation function (login) in userController working correctly

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,21 @@ class UserController {
     return res.json(token);
   }
 
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal('User is not defined'));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('Incorrect password'));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json(token);
+  }
 
   async check(req, res, next) {
     const { id } = req.query;
