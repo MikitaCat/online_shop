53c6719515f276effdbac09cea953e231f5b5cf6
MikitaCat <nikita.proger21@gmail.com>
2021-07-20 20:10:40 +0300

    Routing component and file with constants were created

---
+++ frontend/src/components/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+
+function AppRouter() {
+  const isAuth = false;
+  return (
+    <Switch>
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => {
+          return <Route key={path} path={path} component={Component} exact />;
+        })}
+
+      {publicRoutes.map(({ path, Component }) => {
+        return <Route key={path} path={path} component={Component} exact />;
+      })}
+    </Switch>
+  );
+}
+
+export default AppRouter;
